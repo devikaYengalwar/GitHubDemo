b426c3c93c20d5fc4bf143dd91690978db486cde
jayditiwale <jtiwale@gmail.com>
2024-11-29 23:26:12 -0800

    demo7 added

--- src/Sample/demo4.java
+++ src/Sample/demo4.java
@@ -1,5 +1,9 @@
 package Sample;
 
-public class demo4 {
-
+public class demo4 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("running demo4 ");
+	}
 }
---
+++ src/Sample/demo7.java
@@ -0,0 +1,9 @@
+package Sample;
+
+public class demo7 
+{
+public static void main(String[] args) 
+{
+	System.out.println("testing");
+}
+}
